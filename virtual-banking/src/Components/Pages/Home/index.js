9246c14391ef3b7614242f1a7761de634bdb21bf
Shambhavi Sharma <60618802+ShambhaviSharma0110@users.noreply.github.com>
2021-03-25 19:38:25 +0530

    Add files via upload

--- virtual-banking/src/Components/Pages/Home/index.js
+++ virtual-banking/src/Components/Pages/Home/index.js
@@ -2,7 +2,7 @@ import React, {useState} from 'react';
 import Navbar from '../../Navbar/index';
 import Sidebar from '../../Sidebar/index';
 import Info from '../../Info/Info';
-import { homeObjOne } from '../../Info/Data';
+//import { homeObjOne } from '../../Info/Data';
 const Home = () => {
      const[isOpen,setIsOpen] = useState(false)
      const toggle = () => {
@@ -13,7 +13,6 @@ const Home = () => {
         <div>
             <Sidebar isOpen = {isOpen} toggle={toggle}/>
             <Navbar toggle={toggle} />
-            <Info {...homeObjOne} />
         </div>
     )
 }
